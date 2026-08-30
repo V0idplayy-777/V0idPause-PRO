3d64f2b72675b7749ae8dcb36426f1440865f570
V0idplayy <nburfootfowke@gmail.com>
2026-08-30 14:15:22 +1200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/V0idPause-PRO/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
